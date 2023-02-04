d96b173db65f0a1a2e8b3a32ad41864fe1e5510f
Clark Tolosa <clarktolosa@gmail.com>
2023-02-04 22:21:45 +0800

    nft-preview-card done

--- nft-preview-card/tailwind.config.js
+++ nft-preview-card/tailwind.config.js
@@ -1,8 +1,27 @@
+const plugin = require('tailwindcss/plugin')
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'softBlue': 'hsl(215, 51%, 70%)',
+        'cyan': 'hsl(178, 100%, 50%)',
+        'vdbMain': 'hsl(217, 54%, 11%)',
+        'vdbCard': 'hsl(216, 50%, 16%)',
+        'vdbLine': 'hsl(215, 32%, 27%)',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addBase }) {
+      addBase({
+        ':root': {
+          fontSize: '18px',
+        }
+      })
+    }),
+  ]
 }
